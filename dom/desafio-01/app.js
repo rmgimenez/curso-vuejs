50ce676db5062dc43065257b417c90a6416377c2
Ricardo Gimenez <rmgimenez@gmail.com>
2018-12-14 12:14:22 -0200

    outras formas de resolver o desafio 1

--- dom/desafio-01/app.js
+++ dom/desafio-01/app.js
@@ -6,6 +6,9 @@ new Vue({
         imagem: 'https://s2.glbimg.com/MwoCOU9ZeSnyIAbki1CX0t39vBI=/0x0:524x280/540x304/smart/http://s2.glbimg.com/UpeLroG9Asa2OuGq5mzoeK50evQ=/0x0:524x280/524x280/s.glbimg.com/es/ge/f/original/2016/06/17/carrossel_enquete_280.jpg'
     },
     methods: {
+        idadeVezes3() {
+            return this.idade * 3
+        },
         aleatorio: function() {
             return Math.random()
         }
