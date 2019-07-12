7416bbdae69f5180fa79ac4f6a5ff49170082634
Ricardo Gimenez <rmgimenez@gmail.com>
2019-07-11 23:30:54 -0300

    aula 238 - criando um interceptor para interceptar requisições

--- http-exercicios/src/plugins/axios.js
+++ http-exercicios/src/plugins/axios.js
@@ -6,5 +6,10 @@ axios.defaults.baseURL = 'https://curso-vue-21647.firebaseio.com/'
 Vue.use({
     install(Vue) {
         Vue.prototype.$http = axios
+
+        Vue.prototype.$http.interceptors.request.use(config => {
+            console.log(config.method)
+            return config
+        })
     }
 })
