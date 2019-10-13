a4da6ecfb3d21dd80ade3159a74facad30eeb994
Ricardo Gimenez <rmgimenez@gmail.com>
2019-10-13 19:19:53 -0300

    303 - criando função para comprar uma ação

--- stock-trader/src/components/stocks/Stock.vue
+++ stock-trader/src/components/stocks/Stock.vue
@@ -2,7 +2,7 @@
   <v-flex class="pr-3 pb-3" xs12 md6 lg4>
     <v-card class="green darken-3 white--text">
       <v-card-title class="headline">
-        <strong>NOME <small>(Preço: PREÇO)</small></strong>
+        <strong>{{stock.name}} <small>(Preço: {{stock.price}})</small></strong>
       </v-card-title>
     </v-card>
     <v-card>
@@ -10,8 +10,12 @@
         <v-text-field
           label="Quantidade"
           type="number"
+          v-model.number="quantity"
         ></v-text-field>
-        <v-btn class="green darken-3 white--text">Comprar</v-btn>
+        <v-btn 
+          @click="buyStock"
+          :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+          class="green darken-3 white--text">Comprar</v-btn>
       </v-container>
     </v-card>
   </v-flex>
@@ -19,7 +23,23 @@
 
 <script>
 export default {
-
+  props: ['stock'],
+  data() {
+    return {
+      quantity: 0
+    }
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      }
+      console.log(order)
+      this.quantity = 0
+    }
+  }
 }
 </script>
 
